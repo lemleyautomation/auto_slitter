045667921a62ed6f8cca6ee5b548f888a3d483f8
lemleyautomation <lemley.automation@gmail.com>
2021-10-27 13:38:11 -0400

    changed hmi register setup

--- hmi_server.py
+++ hmi_server.py
@@ -11,7 +11,7 @@ import network_functions as lan
 
 hmi = mod.ModbusTcp(ip="192.168.1.45", port=502)
 hmi.connect()
-hmi_registers = hmi.read_registers(0,39)
+hmi_registers = hmi.read_registers(0,56)
 
 pi_tags = [ [Tags(),False],
             [Tags(),False],
@@ -25,7 +25,7 @@ pi_tags = [ [Tags(),False],
             [Tags(),False] ]
 
 while True:
-    hmi_registers = hmi.read_registers(0,40)
+    hmi_registers = hmi.read_registers(0,56)
 
     pi_tags[2] = lan.update_hmi(pi_tags[2][0], '192.168.1.22')
     pi_tags[3] = lan.update_hmi(pi_tags[3][0], '192.168.1.23')
@@ -42,15 +42,25 @@ while True:
         
         if tags.id != 0:
             r = (tags.id-1)*4
-            hmi_registers[r] = int(abs((tags.deviation+10)*100))
-            hmi_registers[r+1] = int(abs(tags.speed*100))
-            hmi_registers[r+2] = int(abs(tags.servo_offsets[tags.id]+256))
+
+            signed_position = tags.position-(42949672.96*(tags.position>21474836.48))
+            signed_start_position = tags.start_position - (42949672.96*(tags.start_position>21474836.48))
+            relative_position = signed_position - signed_start_position
+
+            position_command = relative_position - tags.deviation
+
+            hmi_registers[r]   = int( abs( (relative_position*100)+1000 ) )
+            hmi_registers[r+1] = int( abs( (position_command*100)+1000 ) )
+            hmi_registers[r+1] = int( abs( (tags.deviation*100)+1000 ) )
+            hmi_registers[r+1] = int( abs( (tags.speed*100)+1000 ) )
+            hmi_registers[r+2] = int( abs( (tags.servo_offsets[tags.id]*100)+1000 ) )
             hmi_registers[r+3] = write_bit(hmi_registers[r+3], 0, status)
             hmi_registers[r+3] = write_bit(hmi_registers[r+3], 1, tags.servo_ready)
             hmi_registers[r+3] = write_bit(hmi_registers[r+3], 2, tags.underspeed)
+            
     
     #pw((hmi_registers[37], hmi_registers[38]))
-    hmi.write_registers(0, hmi_registers[0:36])
+    hmi.write_registers(0, hmi_registers[0:53])
     sleep(0.2)    
 
 hmi.close()
